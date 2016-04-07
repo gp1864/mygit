dcbee0e880db300c5870faed2963b96cc55ce773
U-lisja6\lisja <gp1864+github@gmail.com>
2016-04-06 20:23:53 -0500

    Removed y

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -3,8 +3,8 @@ import java.util.HashMap;
 public class FizzBuzz {
     public String parse(Integer i) {
         HashMap<Integer, String> maps = new HashMap<Integer, String>();
-        maps.put(3, "Fizzy");
-        maps.put(5, "Buzzy");
+        maps.put(3, "Fizzyy");
+        maps.put(5, "Buzzyy");
 
         StringBuffer sb = new StringBuffer();
         for (Integer key : maps.keySet()) {
